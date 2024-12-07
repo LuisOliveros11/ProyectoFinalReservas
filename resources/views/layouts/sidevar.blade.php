44dc44da0ef6a7e4461ab4942f1cb294f1ffdecc
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-12-06 22:38:27 -0700

    Rutas de vistas en el sidebar

--- resources/views/layouts/sidevar.blade.php
+++ resources/views/layouts/sidevar.blade.php
@@ -3,31 +3,31 @@
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="./home">
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
                     Dashboard
                 </a>
                 <div class="sb-sidenav-menu-heading">Panel de mesas</div>
 
-                <a class="nav-link" href="">
+                <a class="nav-link" href="./panelMesas">
                     <div class="sb-nav-link-icon"><i class="fas fa-utensils"></i></div>
                     Mesas
                 </a>
                 <div class="sb-sidenav-menu-heading">Panel de reservas</div>
 
-                <a class="nav-link" href="">
+                <a class="nav-link" href="./panelReservas">
                     <div class="sb-nav-link-icon"><i class="fas fa-calendar-alt"></i></div>
                     Reservas
                 </a>
                 <div class="sb-sidenav-menu-heading">Panel de clientes</div>
 
-                <a class="nav-link" href="">
+                <a class="nav-link" href="./panelClientes">
                     <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                     Clientes
                 </a>
                 <div class="sb-sidenav-menu-heading">Panel de usuarios</div>
 
-                <a class="nav-link" href="">
+                <a class="nav-link" href="./panelUsuarios">
                     <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                     Usuarios
                 </a>
