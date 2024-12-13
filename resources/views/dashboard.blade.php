2302c7a74b9ba02d4264c70e8ac4d6e19523d7e7
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-12-12 21:13:09 -0700

    FrontEnd: Llenado de datos Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,104 +24,107 @@
         @include('layouts.sidevar')
 
         <div id="layoutSidenav_content">
-            <main class="container mt-4">
-                <h1>Dashboard</h1>
-                <div class="d-flex flex-row justify-content-between align-items-stretch">
-                    <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-9 col-lg-10">
-                                    <div class="h6 fw-normal text-gray mb-2">Clientes</div>
+            <div id="app">
+                <main class="container mt-4">
+                    <h1>Dashboard</h1>
+                    <div class="d-flex flex-row justify-content-between align-items-stretch">
+                        <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9 col-lg-10">
+                                        <div class="h6 fw-normal text-gray mb-2">Clientes</div>
+                                    </div>
+                                    <div class="col-3 col-lg-2">
+                                        <div class="sb-nav-link-icon "><i class="fas fa-users"></i></div>
+                                    </div>
                                 </div>
-                                <div class="col-3 col-lg-2">
-                                    <div class="sb-nav-link-icon "><i class="fas fa-users"></i></div>
-                                </div>
+
+                                <h2 class="h3" v-text="obtener_clientes.length"></h2>
                             </div>
-
-                            <h2 class="h3">452</h2>
                         </div>
-                    </div>
 
-                    <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-9 col-lg-10">
-                                    <div class="h6 fw-normal text-gray mb-2">Mesas en total</div>
+                        <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9 col-lg-10">
+                                        <div class="h6 fw-normal text-gray mb-2">Mesas en total</div>
+                                    </div>
+                                    <div class="col-3 col-lg-2">
+                                        <div class="sb-nav-link-icon "><i class="bi bi-table"></i></div>
+                                    </div>
                                 </div>
-                                <div class="col-3 col-lg-2">
-                                    <div class="sb-nav-link-icon "><i class="bi bi-table"></i></div>
-                                </div>
+                                <h2 class="h3" v-text="obtener_mesas.length"></h2>
                             </div>
-                            <h2 class="h3">30</h2>
                         </div>
-                    </div>
 
-                    <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-9 col-lg-10">
-                                    <div class="h6 fw-normal text-gray mb-2">Usuarios</div>
+                        <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9 col-lg-10">
+                                        <div class="h6 fw-normal text-gray mb-2">Usuarios</div>
 
+                                    </div>
+                                    <div class="col-3 col-lg-2">
+                                        <div class="sb-nav-link-icon "><i class="fas fa-users"></i></div>
+                                    </div>
                                 </div>
-                                <div class="col-3 col-lg-2">
-                                    <div class="sb-nav-link-icon "><i class="fas fa-users"></i></div>
-                                </div>
+                                <h2 class="h3" v-text="obtener_usuarios.length"></h2>
                             </div>
-                            <h2 class="h3">30</h2>
                         </div>
-                    </div>
 
-                    <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-9 col-lg-10">
-                                    <div class="h6 fw-normal text-gray mb-2">Reservaciones</div>
+                        <div class="card shadow mx-2" style="flex: 1; min-width: 200px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9 col-lg-10">
+                                        <div class="h6 fw-normal text-gray mb-2">Reservaciones</div>
 
+                                    </div>
+                                    <div class="col-3 col-lg-2">
+                                        <div class="sb-nav-link-icon "><i class="bi bi-calendar2-check-fill"></i></div>
+                                    </div>
                                 </div>
-                                <div class="col-3 col-lg-2">
-                                    <div class="sb-nav-link-icon "><i class="bi bi-calendar2-check-fill"></i></div>
-                                </div>
+                                <h2 class="h3" v-text="obtener_reservas.length"></h2>
+
                             </div>
-                            <h2 class="h3">30</h2>
-
                         </div>
                     </div>
-                </div>
-                <br>
+                    <br>
 
 
-                <div class="row">
-                    <div class="col-6 col-lg-6 col-md-6">
-                        <h2 class="text-center fs-4 fs-md-3 fs-lg-2">Clientes más Recientes</h2>
+                    <div class="row">
+                        <div class="col-6 col-lg-6 col-md-6">
+                            <h2 class="text-center fs-4 fs-md-3 fs-lg-2">Clientes más Recientes</h2>
 
-                        <div class="table-responsive">
-                            <table class="table table-striped mt-6">
-                                <thead>
-                                    <tr>
-                                        <th class="h6 h-md5 h-lg4">Nombre</th>
-                                        <th class="h6 h-md5 h-lg4">Apellidos</th>
-                                        <th class="h6 h-md5 h-lg4">Fecha de Registro</th>
-                                        <th class="h6 h-md5 h-lg4">Email</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Oscar</td>
-                                        <td>Aguilar</td>
-                                        <td>2024-11-28</td>
-                                        <td>oscar@gmail.com</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div class="table-responsive">
+                                <table class="table table-striped mt-6">
+                                    <thead>
+                                        <tr>
+                                            <th class="h6 h-md5 h-lg4">Nombre</th>
+                                            <th class="h6 h-md5 h-lg4">Apellidos</th>
+                                            <th class="h6 h-md5 h-lg4">Fecha de Registro</th>
+                                            <th class="h6 h-md5 h-lg4">Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="(cliente, index) in obtener_clientes" :key="cliente.id">
+                                            <td v-text="cliente.nombre"></td>
+                                            <td v-text="cliente.apellidos"></td>
+                                            <td v-text="cliente.fecha_registro"></td>
+                                            <td v-text="cliente.correo_electronico"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
 
 
 
 
 
-            </main>
+                </main>
+
+            </div>
         </div>
 
 
@@ -135,6 +138,35 @@
     <script src="assets/demo/chart-area-demo.js"></script>
     <script src="assets/demo/chart-bar-demo.js"></script>
     <script src="assets/demo/chart-pie-demo.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.prod.js"></script>
+
+    <script>
+        const {
+            createApp,
+            ref
+        } = Vue
+
+        createApp({
+            setup() {
+                const obtener_reservas = ref(<?php echo json_encode($reservas['reservaciones']); ?>)
+                const obtener_mesas = ref(<?php echo json_encode($mesas['mesas']); ?>)
+                const obtener_clientes = ref(<?php echo json_encode($clientes['clientes']); ?>)
+                const obtener_usuarios = ref(<?php echo json_encode($usuarios['usuarios']); ?>)
+                return {
+                    obtener_reservas,
+                    obtener_mesas,
+                    obtener_usuarios,
+                    obtener_clientes
+                }
+            },
+            methods: {
+            },
+            mounted() {
+
+            }
+
+        }).mount('#app')
+    </script>
 </body>
 
 
